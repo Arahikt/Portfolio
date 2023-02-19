f1077235f928637d9d961d726a192c79cb223693
Arahik Torosian <arahikt@gmail.com>
2023-02-18 18:04:46 -0800

    Header Updated.

--- src/shared/UIElement/Header.js
+++ src/shared/UIElement/Header.js
@@ -5,20 +5,21 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
-  return (<header>
-    <Navbar bg="black" variant="dark" collapseOnSelect expand="lg">
-      <Container>
-        <Navbar.Brand href="/">Arahik Torosian</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">HOME</Nav.Link>
-            <Nav.Link href="/projects">PROJECTS</Nav.Link>
-            <Nav.Link href="/contact">CONTACT</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  return (
+    <header>
+      <Navbar bg="black" variant="dark" collapseOnSelect expand="lg">
+        <Container>
+          <Navbar.Brand href="/">A T</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">HOME</Nav.Link>
+              <Nav.Link href="/projects">PROJECTS</Nav.Link>
+              <Nav.Link href="/contact">CONTACT</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
