9d300f341775ee2951e3daff2013e63a3663ef88
Arahik Torosian <arahikt@gmail.com>
2023-02-18 18:05:22 -0800

    App.js updated.

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 import Home from "./Home/pages/Home";
 import Projects from "./Home/pages/Projects";
 import Contact from "./Home/pages/Contact";
+import Footer from "./shared/UIElement/Footer";
+import Header from "./shared/UIElement/Header";
 
 const App = () => {
   return (
     <Router>
-      <div className="App scroll">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Navigate to="/"/>} />
-      </Routes>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- src/Home/pages/Home.js
+++ src/Home/pages/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main>
+      <div>
+        <h1>This is home page!!</h1>
+       <div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+          distinctio dolores optio hic nostrum, delectus doloribus officiis
+          alias accusantium reiciendis nam beatae, dicta sapiente voluptatum,
+          totam porro qui maxime natus. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Ullam distinctio dolores optio hic nostrum, delectus
+          doloribus officiis alias accusantium reiciendis nam beatae, dicta
+          sapiente voluptatum, totam porro qui maxime natus. Lorem ipsum, dolor
+          sit amet consectetur adipisicing elit. Ullam distinctio dolores optio
+          hic nostrum, delectus doloribus officiis alias accusantium reiciendis
+          nam beatae, dicta sapiente voluptatum, totam porro qui maxime natus.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+          distinctio dolores optio hic nostrum, delectus doloribus officiis
+          alias accusantium reiciendis nam beatae, dicta sapiente voluptatum,
+          totam porro qui maxime natus.
+        </p>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Home;
