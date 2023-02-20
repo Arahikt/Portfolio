2ce64050e6598d52e83da9a63855dfb9a7d25212
Arahik Torosian <arahikt@gmail.com>
2023-02-20 14:27:06 -0800

    Contact Us page updated.

--- src/Home/pages/Contact.js
+++ src/Home/pages/Contact.js
@@ -1,79 +1,103 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import backgroundContact from "../../Assets/Images/mail-delivery.png";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Contact.css";
 
-const Contact = () => {
-  
-  const [state, setState] = useState({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    email: '',
-    message: ''
-  });
-  const [buttonText, setButtonText] = useState('Send');
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
-  const handleChange = event => {
-    const name= event.target.name;
-    const value = event.target.value;
-      setState({...state,[name]: value})
-  }
+const Contact = (props) => {
+  const formInitial = {
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    message: "",
+  };
+  const [buttonText, setButtonText] = useState("Submit");
+  const [state, setState] = useState(formInitial);
 
+  const handleChange = (event) => {
+    setState((prevState) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }));
+  };
+  const toastifySuccess = () => {
+    toast.success("Message Sent Successfully.", {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
   const submitHandler = async (event) => {
     event.preventDefault();
     setButtonText("Sending");
+    let response
     try {
-      await fetch("http://localhost:5000/contact-us", {
+      response = await fetch("http://localhost:3001/send", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json;charset=utf-8",
       },
       body: JSON.stringify(state),
     });
-    setButtonText("Send");
-    setState("");
-    } catch (error) {}
+      setState(formInitial);
+    } catch (error) {} 
+
+    let responseData = await response.json();
+    if (responseData.status === "success") {
+    //   alert("Message Sent");
+        toastifySuccess();
+    } else if (responseData.status === "fail") {
+      alert("Message failed to send");
+    }
+    setButtonText("Submit");
   };
 
   return (
-
-        <section className="contact-page">
+    <section className="contact-page">
       <Container>
         <Row className="align-items-center">
           <Col size={12} md={6}>
-                <img  src={backgroundContact} alt="Contact Us"/>
+            <img src={backgroundContact} alt="Contact Us" />
           </Col>
           <Col size={12} md={6}>
-     
-                <div >
-                <h2 className="contact-header">Contact Us</h2>
-                <form onSubmit={submitHandler}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={state.firstName} placeholder="First Name" name="firstName" onChange={handleChange} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={state.lastName} placeholder="Last Name" name="lastName" onChange={handleChange}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={state.phone} placeholder="Phone Number" name="phone" onChange={handleChange}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={state.email} placeholder="Email" name="email" onChange={handleChange} />
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={state.message} placeholder="Message" name="message" onChange={handleChange}></textarea>
-                      <button className ="form-button" type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                  </Row>
-                </form>
-              </div>
+            <div>
+              <h2 className="contact-header">Contact Us</h2>
+              <form onSubmit={submitHandler}>
+                <Row>
+                  <Col size={12} sm={6} className="px-1">
+                    <input type="text" placeholder="First Name" value={state.firstName} name="firstName" onChange={handleChange} />
+                  </Col>
+                  <Col size={12} sm={6} className="px-1">
+                    <input type="text" placeholder="Last Name" value={state.lastName} name="lastName" onChange={handleChange} />
+                  </Col>
+                  <Col size={12} sm={6} className="px-1">
+                    <input type="tel" placeholder="Phone Number" value={state.phone} name="phone" onChange={handleChange} 
+                   minLength= "10"/>
+                  </Col>
+                  <Col size={12} sm={6} className="px-1">
+                    <input type="email" placeholder="Email" value={state.email} name="email" onChange={handleChange} />                  
+                  </Col>
+                  <Col size={12} className="px-1">
+                    <textarea rows="6" placeholder="Message" value={state.message} name="message" onChange={handleChange}></textarea>
+
+                    <button disabled={(state.firstName && state.lastName && state.email && state.phone && state.message )=== ""  } className="form-button" type="submit"><span>{buttonText}</span></button>
+                  </Col>
+                </Row>
+                <ToastContainer />
+              </form>
+            </div>
           </Col>
         </Row>
       </Container>
     </section>
-   
   );
 };
 export default Contact;
