0c6f95c1be7e067900cb5ef8ee7a67a54d730674
Arahik Torosian <arahikt@gmail.com>
2023-02-22 13:14:11 -0800

    Project Demo page added.

--- src/Home/components/ProjectDemo.js
+++ src/Home/components/ProjectDemo.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+
+import ReactPlayer from "react-player";
+import comingSoon from '../../Assets/Images/Projects/vecteezy_silver-cinematic-coming-soon-title-animation_9859380_481.mp4';
+import "./ProjectDemo.css";
 
 const ProjectDemo = () => {
   return (
-    <div>ProjectDemo</div>
-  )
-}
+    <div className="player-wrapper">
+     
+        <ReactPlayer
+          url={comingSoon}
+          className="video-player"
+          width="70%"
+          height="70%"
+          controls
+        />
+      
+    </div>
+  );
+};
 
 export default ProjectDemo;
